c9e3dd939612282233677ee35dcfe4bb32650047
Shlokkh7 <u24ec044@eced.svnit.ac.in>
2025-06-04 21:40:46 +0530

    Add files via upload

---
+++ adder/1bit_full_adder/fa_behaviour_tb.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.06.2025 21:17:33
+// Design Name: 
+// Module Name: fa_behaviour_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fa_behaviour_tb(
+
+    );
+
+    reg x;
+    reg y;
+    reg cin;
+    wire s;
+    wire cout;
+    
+    fa_behavioral uut(
+        .x(x),
+        .y(y),
+        .cin(cin),
+        .s(s),
+        .cout(cout)
+    );
+    
+    initial
+    begin
+        #50 $finish;
+    end
+    
+    initial
+    begin
+        x = 1'b0;
+        y = 1'b0;
+        cin = 1'b0;
+        #10
+        
+        x = 1'b0;
+        y = 1'b0;
+        cin = 1'b1;
+        #10
+        
+        x = 1'b0;  
+        y = 1'b1;  
+        cin = 1'b0;
+        #10        
+           
+        x = 1'b0;  
+        y = 1'b1;  
+        cin = 1'b1;
+        #10
+        
+        x = 1'b1;  
+        y = 1'b0;  
+        cin = 1'b0;
+         
+      end
+endmodule
+
+
