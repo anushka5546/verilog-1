85fcf3ffa5a53cf9af4ee173c768ac1d87f490ee
Shlokkh7 <u24ec044@eced.svnit.ac.in>
2025-06-15 19:47:39 +0530

    Update mux_generic_1bit.v

--- mux/Nx1/mux_generic_1bit.v
+++ mux/Nx1/mux_generic_1bit.v
@@ -23,19 +23,17 @@
 module mux_generic_1bit
     #(parameter INS = 5)
     (
-    input [INS - 1:0] w,
-    input [$clog2(INS) - 1:0] s,
-    output reg f
+    input [INS - 1:0] w,                      
+    input [$clog2(INS) - 1:0] s,               
+    output reg f                                      
     );
-    
+
     integer k;
-    always @(w , s)
+    always @(*) 
     begin
-        f = 'bx;
-        for(k = 0; k < INS; k = k + 1)
-        begin
+        f = 'bx; 
+        for (k = 0; k < INS; k = k + 1)
             if (k == s)
                 f = w[k];
-        end      
     end
 endmodule
