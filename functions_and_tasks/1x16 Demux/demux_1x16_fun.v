f9fea1c69f51114a265478e4503d543658269ac9
Shlokkh7 <u24ec044@eced.svnit.ac.in>
2025-06-19 19:42:32 +0530

    Create demux_1x16_fun.v

---
+++ functions_and_tasks/1x16 Demux/demux_1x16_fun.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+module demux_1x16_fun(
+    input i_f,
+    input [3:0] i_s,
+    output [15:0] o_y
+    );
+    
+    function [15:0] demux_1x16;
+        input f;
+        input [3:0] s;
+        integer k;
+        begin
+            demux_1x16 = 16'd0;
+            for(k = 0; k < 16; k = k + 1) begin
+                if (k == s)
+                    demux_1x16[k] = f;
+            end
+        end
+    endfunction
+    
+    assign o_y = demux_1x16(i_f, i_s);
+endmodule
